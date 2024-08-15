fb5150dc371667e647e6eeca98e6be147193ec14
ozge belen <ozgebelen@ozge-MacBook-Pro.local>
2024-08-15 17:11:02 +0300

    bottom Tabs

--- App.js
+++ App.js
@@ -1,36 +1,43 @@
-import { Text } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
-import {createDrawerNavigator} from '@react-navigation/drawer'
-import UserScreen from './screens/UserScreen';
-import WelcomeScreen from './screens/WelcomeScreen';
-import {Ionicons} from '@expo/vector-icons'
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import UserScreen from "./screens/UserScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
-  return(
-
+  return (
     //  <Text>Todoo..</Text>
     <NavigationContainer>
-    <Drawer.Navigator initialRouteName='Welcome' screenOptions={{
-      headerStyle: {backgroundColor: '#3c0a6b'},
-      headerTintColor: 'white',
-      drawerActiveBackgroundColor: '#7e84f7',
-      drawerActiveTintColor: 'white',
-     drawerStyle: {backgroundColor: '#ccc'}
-    }}>
-      <Drawer.Screen name='Welcome' component={WelcomeScreen} options={{
-        drawerLabel: 'Welcome Screen',
-        drawerIcon: ({color, size})=>(<Ionicons name='home' color={color} size={size}/> )
-       
-      }}/>
-      <Drawer.Screen name='User' component={UserScreen} options={{
-        //Parametrelerin aynısını verdiğimizde lçüyü otomatik olarak ayarlar
-        drawerIcon: ({color, size})=> <Ionicons name='person' color={color} size={size}/>  
-      }}/>
-    </Drawer.Navigator>
-
-  </NavigationContainer>
-) 
+      <BottomTab.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3c0a6b" },
+          headerTintColor: "white",
+          tabBarActiveTintColor: '#3c0a6b'
+        }}
+      >
+        <BottomTab.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),}}
+        />
+        <BottomTab.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            //Parametrelerin aynısını verdiğimizde lçüyü otomatik olarak ayarlar
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
+  );
 }
-
